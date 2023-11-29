812a712e21d96e33a277af028657161961ba3f2e
JOHN <johnoluwaseun2020@gmail.com>
2023-11-29 16:28:54 +0000

    work

---
+++ Classone/script.js
@@ -0,0 +1,9 @@
+var firstName = Number (prompt("Enter the firstName"))
+var secondName =Number (prompt("Enter the secondName"))
+var answer = firstName + secondName
+console.log(answer);
+
+var firstName =  prompt("Enter the firstName")
+var secondName = prompt("Enter the secondName")
+var answer = firstName + secondName
+console.log(answer);
---
+++ Conditional Statement/script.js
@@ -0,0 +1,34 @@
+// var drink = "fanta"
+
+// if(drink == "pepsi") {
+//     result.innerHTML = "buy fanta"
+// } else {
+//     result.innerHTML = "just buy anything"
+// }
+
+// var food = "Eba"
+
+// if( food =="Amala") {
+//     console.log(food);
+//     result.innerHTML = "Buy Abula"
+// } else if(food == "Eba") {
+//     result.innerHTML = "Buy ila/egusi"
+// } else if(food == "Iyan") {
+//     result.innerHTML ="Buy efo riro/egusi"
+// } else {
+//     result.innerHTML = "Buy me bread and beans with chilled fanta"
+// }
+
+// if (45==9) {
+//     result.innerHTML = "Yes"
+// }else {
+//     result.innerHTML = "No"
+// }
+
+
+function John(params) {
+    
+}
+    
+    
+ 
